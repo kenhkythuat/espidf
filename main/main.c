1b3626497e0d58decbb5955144add8b4ff27690f
kenhkythuat <thuanphat7.embedded@gmail.com>
2024-05-06 12:22:59 +0700

    inital code solenoid

--- main/main.c
+++ main/main.c
@@ -15,14 +15,6 @@
 
 void app_main()
 {
-    // configure_led(BLINK_GPIO);
-
     esim_config();
     config_pwm_50hz();
-
-    //     TaskHandle_t xHandle = NULL;
-    //     xTimers[0] = xTimerCreate("bink led", pdMS_TO_TICKS(1000), pdTRUE, (void *)0, vTimerCallback);
-    //     xTimerStart(xTimers[0], 0);
-    //   xTaskCreate(spwm_task, "spwm_task", 2048, NULL, configMAX_PRIORITIES - 2, NULL);
-    //     xTaskCreate(status_led, "status_led", 2048, NULL, configMAX_PRIORITIES - 3, NULL);
 }
